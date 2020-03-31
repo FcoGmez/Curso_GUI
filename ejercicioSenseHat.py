5ca8ae24579ba3309c3142c543e21704e11dfe95
fjgomez <fjgcampos95@gmail.com>
2020-03-31 03:21:05 +0200

    Primera fase del ejercicio entregable SenseHat

---
+++ ejercicioSenseHat.py
@@ -0,0 +1,128 @@
+from sense_emu import SenseHat # pip3 install sense_emu
+sense= SenseHat()
+import time 
+import tkinter as tk 
+import tkinter.ttk as ttk
+from ttkthemes import ThemedStyle
+from tkinter.messagebox import showinfo
+from tkinter import messagebox as mb
+
+valor=0
+
+
+class Aplicacion:
+    def __init__(self):
+        self.i=0
+        self.imax=100
+        self.ventana=tk.Tk()
+        style = ThemedStyle(self.ventana)
+        style.set_theme("ubuntu")
+        self.ventana.title("Práctica GUI SenseHat")
+        self.cuaderno = ttk.Notebook(self.ventana)
+
+        self.pagina1 = ttk.Frame(self.cuaderno)
+        self.cuaderno.add(self.pagina1, text='Monitorización')
+        
+        self.labelframe1=ttk.LabelFrame(self.pagina1, text='Control')
+        self.labelframe1.grid(column=0, row=0, sticky="WE")
+        self.control()
+
+        self.labelframe2=ttk.LabelFrame(self.pagina1, text='Medidas')
+        self.labelframe2.grid(column=0, row=1, sticky='WE')
+        self.medidas()
+
+        self.labelframe3=ttk.LabelFrame(self.pagina1, text='Histórico')
+        self.labelframe3.grid(column=0, row=2, sticky='WE')
+        self.historico()
+
+
+
+
+        self.pagina2 = ttk.Frame(self.cuaderno)
+        self.cuaderno.add(self.pagina2, text='Grafica')
+        
+
+        #self.ventana.geometry('600x600')
+        #self.ventana.minsize(height=600, width=400)
+        self.cuaderno.grid(column=0, row=0, sticky='WE')
+        self.ventana.mainloop()
+        
+        
+    def control(self):
+        self.label1=ttk.Label(self.labelframe1, text="Aqui se implementará un boton de inicio")
+        self.label1.grid(column=0, row=0, sticky='WE')
+
+        self.boton1=ttk.Button(self.labelframe1, text="Obtener valor:", command=self.obtenervalor)
+        self.boton1.grid(column=1, row=2)
+
+    def medidas(self):
+        self.label2=ttk.Label(self.labelframe2, text="Aqui se implementará el valor del sensor a leer")
+        self.label2.grid(column=0, row=0, sticky='WE')
+
+        self.lectura=ttk.Label(self.labelframe2)
+        self.lectura.configure(foreground='blue')
+        self.lectura.grid(column=0, row=1)
+
+        self.seleccion=tk.IntVar()
+        self.seleccion.set(3)
+        self.radio1=ttk.Radiobutton(self.labelframe2,text="Temperatura",variable=self.seleccion, value=1)
+        self.radio1.grid(column=0,row=3)
+        self.radio2=ttk.Radiobutton(self.labelframe2,text="Presion",variable=self.seleccion, value=2)
+        self.radio2.grid(column=1,row=3)
+        self.radio3=ttk.Radiobutton(self.labelframe2,text="Humedad",variable=self.seleccion, value=3)
+        self.radio3.grid(column=2,row=3) 
+
+        
+
+    def historico(self):
+        self.label3=ttk.Label(self.labelframe3, text="Aqui se implementará un historico de los datos")
+        self.label3.grid(column=0, row=0, sticky='WE')
+
+
+    def obtenervalor(self):
+
+        self.lecturasensor()
+    
+
+    def lecturasensor(self):
+        
+        if self.seleccion.get() == 1:
+            valor = sense.temp
+            self.lectura.configure(text=str(valor)+' ºC')
+            
+        elif self.seleccion.get() ==2:
+            valor = sense.pressure
+            self.lectura.configure(text=str(valor)+' mbar')
+            
+        elif self.seleccion.get() ==3:
+            valor = sense.humidity
+            self.lectura.configure(text=str(valor)+' %')
+        else:
+            valor=0
+            self.lectura.configure(text=str(valor))    
+        
+        
+
+
+aplicacion=Aplicacion()
+
+
+
+
+
+
+
+
+
+#while self.i < self.imax : 
+#    pinta=str(self.i)
+#    print(pinta)
+#    self.lecturasensor()
+#    self.i=self.i + 1
+#    time.sleep(1)
+
+
+
+#temp= sense.temp
+#pre = sense.pressure
+#hum= sense.humidity
