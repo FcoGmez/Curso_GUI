c0fdb7edd7d42c860e0f7d5dd7e49f1d48dc4832
fjgomez <fjgcampos95@gmail.com>
2020-04-10 05:16:54 +0200

    Archivo de pruebas

--- pruebas.py
+++ pruebas.py
@@ -1,56 +1,27 @@
-from sense_emu import SenseHat # pip3 install sense_emu
-sense= SenseHat()
-import time 
-import tkinter as tk 
-import tkinter.ttk as ttk
-from ttkthemes import ThemedStyle
-from tkinter.messagebox import showinfo
-from tkinter import messagebox as mb
-
-valor=0
+import tkinter as tk
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
 
 class Aplicacion:
     def __init__(self):
-        self.i=0
-        self.imax=100
         self.ventana=tk.Tk()
-        style = ThemedStyle(self.ventana)
-        style.set_theme("ubuntu")
-        self.ventana.title("Práctica GUI SenseHat")
+        self.fig = Figure()
+    
+        self.ax = self.fig.add_subplot(111) # https://matplotlib.org/3.1.3/api/_as_gen/matplotlib.pyplot.subplot.html: nºfilas, nºcolumnas, índice subplotcd
+        self.ax.set_xlabel("X axis")
+        self.ax.set_ylabel("Y axis")
+        self.ax.cla()   # clear axis
+        self.ax.grid()  # configura grid
+        self.ax.set_xlim(-10, 10)
+        self.ax.set_ylim(0, 10)
 
-        self.lectura=ttk.Label(self.ventana)
-        self.lectura.configure(foreground='blue')
-        self.lectura.grid(column=1, row=2)
+        x = range(-10,11)
+        y = [v*v for v in x]
+        self.line, = self.ax.plot(x, y, marker='o', color='orange')
 
-        self.seleccion=tk.IntVar()
-        self.seleccion.set(3)
-        self.radio1=ttk.Radiobutton(self.ventana,text="Temperatura",variable=self.seleccion, value=1)
-        self.radio1.grid(column=0,row=3)
-        self.radio2=ttk.Radiobutton(self.ventana,text="Presion",variable=self.seleccion, value=2)
-        self.radio2.grid(column=1,row=3)
-        self.radio3=ttk.Radiobutton(self.ventana,text="Humedad",variable=self.seleccion, value=3)
-        self.radio3.grid(column=2,row=3) 
-
-        self.lecturasensor()
+        self.graph = FigureCanvasTkAgg(self.fig, master=self.ventana)   # Agg: Anti-Grain geometry rendering engine
+        self.graph.get_tk_widget().pack(side="top",fill='both',expand=True)        
 
         self.ventana.mainloop()
 
-    def lecturasensor(self):
-        
-        if self.seleccion.get() == 1:
-            valor = sense.temp
-            self.lectura.configure(text=str(valor))
-            
-        elif self.seleccion.get() ==2:
-            valor = sense.pressure
-            self.lectura.configure(text=str(valor))
-            
-        elif self.seleccion.get() ==3:
-            valor = sense.humidity
-            self.lectura.configure(text=str(valor))
-        else:
-            valor=0
-            self.lectura.configure(text=str(valor))    
-        
-
-aplicacion=Aplicacion()
+Aplicacion()
